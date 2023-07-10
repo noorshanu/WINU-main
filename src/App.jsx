ffc3b90e4e6c8523a05dc7fcc5897e4890ac4243
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-10 19:11:43 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <Navbar/>
     </>
   )
 }
