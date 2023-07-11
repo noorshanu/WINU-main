ac55148a688a8666351770b20e9a7f01dbb8288e
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-11 12:14:01 +0530

    token section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
+import Token from './sections/Token'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero/>
+      <Token/>
     </>
   )
 }
---
+++ src/sections/Token.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Token() {
+  return (
+    <section>
+        <div className=' mx-auto max-w-7xl '>
+            <div className='text-center text-3xl sm:text-5xl'>
+                <h1 className='text-[#fff] font-bold mb-4'>Tokenomics</h1>
+                <div className='bg-[#fff] w-[96px] h-[2px] mx-auto'></div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Token
