6a4caf9fe2245e7e601c7b9eef91e5d5cb9924c7
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-11 14:10:59 +0530

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import {BsTwitter,BsFacebook} from 'react-icons/bs'
 
 function Footer() {
   return (
-    <section className=' mt-12'>
+    <section className=" mt-12">
+      <div className="mx-auto text-center relative">
+        <img src="/assets/logos/text-logo.svg" alt="" className="mx-auto z-40" />
 
-    <div className='mx-auto text-center'>
-        <img src="/assets/logos/text-logo.svg" alt=""  className='mx-auto'/>
+        <p className="text-white text-sm font-[500] mt-3 z-40">
+          Clarity gives you the blocks and components you need to <br /> create
+          a truly professional website.
+        </p>
+       
+      </div>
 
-        <p className='text-white text-sm font-[500] mt-3'>Clarity gives you the blocks and components you need to <br/> create a truly professional website.</p>
-        </div>    
+      <div className="relative mt-[16rem]">
+    <div className="black-blur">
+
+    </div>
+        <img
+          src="images/footer-bg.png"
+          alt=""
+          className="w-full absolute bottom-0"
+        />
+        <img
+          src="images/footer-bt.png"
+          alt=""
+          className=" absolute bottom-0 w-full z-10"
+        />
+        <div className="">
+            <div className="flex">
+                <a href=""><BsTwitter/></a>
+                <a href=""><BsFacebook/></a>
+
+            </div>
+
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
