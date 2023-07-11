4093a71df8f17cde443282fd8d5af4fe7dcf9dae
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-11 13:10:45 +0530

    partners

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import HowTobuy from './sections/HowTobuy'
+import Partners from './sections/Partners'
 import Token from './sections/Token'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <Token/>
       <HowTobuy/>
+      <Partners/>
     </>
   )
 }
