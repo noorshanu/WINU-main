c062bd8425a22f1b32a213ef7f66b652d9b2affd
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-11 12:24:37 +0530

    formatted

--- src/sections/Token.jsx
+++ src/sections/Token.jsx
@@ -1,22 +1,22 @@
-
-
 function Token() {
   return (
     <section>
-        <div className=' mx-auto max-w-7xl '>
-            <div className='text-center text-3xl sm:text-5xl'>
-                <h1 className='text-[#fff] font-bold mb-4'>Tokenomics</h1>
-                <div className='bg-[#fff] w-[96px] h-[2px] mx-auto'></div>
-            </div>
-
-            <div className="text-center ml-44 mt-24">
-                <img src="assets/images/token.png" alt="" className="m-auto text-center" />
-
-            </div>
-
+      <div className=" mx-auto max-w-7xl ">
+        <div className="text-center text-3xl sm:text-5xl">
+          <h1 className="text-[#fff] font-bold mb-4">Tokenomics</h1>
+          <div className="bg-[#fff] w-[96px] h-[2px] mx-auto"></div>
         </div>
+
+        <div className="text-center ml-44 mt-24">
+          <img
+            src="assets/images/token.png"
+            alt=""
+            className="m-auto text-center"
+          />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Token
+export default Token;
